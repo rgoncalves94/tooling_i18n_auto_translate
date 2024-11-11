72eef37f297bcb6248a106b062930436b8beb331
Rafael Gonçalves <rafael.goncalves.dev@outlook.com>
2024-11-10 23:29:39 -0300

    feat: initial commit

---
+++ example/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <h1>{i18n.gettext("Good evening")}</h1>;
+};
+
+export { Footer };
---
+++ example/app/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h1>{i18n.gettext("Good morning")}</h1>;
+};
+
+export { Header };
